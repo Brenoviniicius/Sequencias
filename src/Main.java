443e7a1c251312a7ffc7e6c2b8be232fd6faa8a2
Breno Vinicius <brenovmsilva82@gmail.com>
2023-05-02 22:33:20 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
+public class Main  {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // sequência a)
+        int proximo = 9;
+        System.out.println("a) Próximo elemento: " + proximo);
+
+        // sequência b)
+        proximo = 128;
+        System.out.println("b) Próximo elemento: " + proximo);
+
+        // sequência c)
+        proximo = 49;
+        System.out.println("c) Próximo elemento: " + proximo);
+
+        // sequência d)
+        proximo = 100;
+        System.out.println("d) Próximo elemento: " + proximo);
+
+        // sequência e)
+        proximo = 13;
+        System.out.println("e) Próximo elemento: " + proximo);
+
+        // sequência f)
+        proximo = 20;
+        System.out.println("f) Próximo elemento: " + proximo);
     }
 }
